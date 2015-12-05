49f86f5841c1b52adaadb10d715ae73158430135
dhruv chauhan <dc14690@my.bristol.ac.uk>
2015-12-05 07:11:12 +0000

    working on php files

--- web_files/add_challenge.php
+++ web_files/add_challenge.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con = mysql_connect("0.0.0.0", "root", "");
+	$con = mysql_connect("http://ec2-54-74-45-107.eu-west-1.compute.amazonaws.com", "root", "");
 
 	if (!$con) {
 		die('Could not connect: ' . mysql_error());
--- web_files/get_challenges.php
+++ web_files/get_challenges.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysql_connect("0.0.0.0", "root", "")
+	$con = mysql_connect("http://ec2-54-74-45-107.eu-west-1.compute.amazonaws.com", "root", "")
 
 	if (!$con) {
 		die('Could not connect: ' . mysql_error());
@@ -8,7 +8,7 @@
 	mysql_select_db("db", $con);
 
 	// stars in a table in the db bigstars.
-	$result = mysql_query("SELECT * FROM challenges");
+	$result = mysql_query("SELECT * FROM challenge WHERE userID = ");
 
 	while ($row = mysql_fetch_assoc($result)) {
 		$output[] = $row;
